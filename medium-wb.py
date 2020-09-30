fec32322a59fcfabfc3d330bc44c1d220d2cccec
Erin Leeds <erin@erinleeds.com>
2020-09-30 18:41:18 +0000

    decoded a string

--- medium-wb.py
+++ medium-wb.py
@@ -5,6 +5,7 @@ def is_anagram_of_palindrome(word):
 
     for char in word:
         count = ht.get(char, 0) +1 
+        ht[char] = count
 
     seen_odd = False
     for value in ht.values():
@@ -14,3 +15,31 @@ def is_anagram_of_palindrome(word):
     return seen_odd
 
 print(is_anagram_of_palindrome("abbca"))
+
+def balanced_parens(phrase):
+
+    parens = 0
+
+    for char in phrase:
+        if char == "(":
+            parens+=1
+        elif char == ")":
+            parens -=1
+        elif parens == -1:
+            return False
+    return parens == 0
+
+def decode(phrase):
+
+    i = 0
+    decoded = ''
+    while i < len(s):
+        num_to skip = phrase[i] + 1
+        decoded += phrase[num_to_skip]
+
+        i+=1
+
+    return decoded
+
+
+
