c91c533b4daf433915d987ef6e6d3804d830ff97
Erin Leeds <erin@erinleeds.com>
2020-10-11 21:11:51 +0000

    remove_node

--- linked-list.py
+++ linked-list.py
@@ -58,7 +58,31 @@ class LinkedList:
                 return
             cur_idx += 1
         
+class Node(object):
+    """Class in a linked list."""
 
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def as_string(self):
+        """Represent data for this node and it's successors as a string.
+
+        >>> Node(3).as_string()
+        '3'
+
+        >>> Node(3, Node(2, Node(1))).as_string()
+        '321'
+        """
+
+        out = []
+        n = self
+
+        while n:
+            out.append(str(n.data))
+            n = n.next
+
+        return "".join(out)
         
 
 # my_list = LinkedList()
@@ -66,3 +90,20 @@ class LinkedList:
 # brownie = Node("yum")
 # my_list.append(1)
 # my_list.display()
+
+def remove_node(node):
+    """Given a node in a linked list, remove it.
+
+    Remove this node from a linked list. Note that we do not have access to
+    any other nodes of the linked list, like the head or the tail.
+
+    Does not return anything; changes list in place.
+    """
+
+    # START SOLUTION
+
+    if not node.next:
+        raise ValueError("Cannot remove tail node")
+
+    node.data = node.next.data
+    node.next = node.next.next
