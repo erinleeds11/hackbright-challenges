114502984dcd5cf65eb8edc1e6f42c6c8ab53e90
Erin Leeds <erin@erinleeds.com>
2020-10-18 21:04:59 +0000

    selection sort

--- sorts.py
+++ sorts.py
@@ -13,4 +13,15 @@ def best_bubble_sort(lst):
             # If no swap, list already sorted
             break
 
-def sel
+def selectionSort(alist):
+   for fillslot in range(len(alist)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if alist[location]>alist[positionOfMax]:
+               positionOfMax = location
+
+       temp = alist[fillslot]
+       alist[fillslot] = alist[positionOfMax]
+       alist[positionOfMax] = temp
+
+
