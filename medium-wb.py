260882971225269c5c2661e50583a7b65add4bdb
Erin Leeds <erin@erinleeds.com>
2020-10-18 19:52:40 +0000

    count recursively

--- medium-wb.py
+++ medium-wb.py
@@ -75,4 +75,35 @@ def balenced_parens(s):
                 parents -=1
     return parens == 0
 
-def binary_search()
+def binary_search(val):
+    """Using binary search, find val in range 1-100. Return # of guesses."""
+
+    assert 0 < val < 101, "Val must be between 1-100"
+
+    num_guesses = 0
+
+    higher_than  = 0
+    lower_than = 101
+    guess = None
+
+    while guess != val:
+        num_guesses += 1
+        guess = (lower_than - higher_than) // 2 + higher_than
+
+        if val > guess:
+            higher_than = guess
+
+        elif val < guess:
+            lower_than = guess
+
+
+
+    return num_guesses
+
+def count_recursively(lst):
+    """Return number of items in a list, using recursion."""
+
+     if not lst:
+        return 0
+
+    return 1 + count_recursively(lst[1:])
